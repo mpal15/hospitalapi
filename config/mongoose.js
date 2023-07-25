160660035e641a1d05c3039bb3f7a918d7eacd52
Mohit Pal <62149463+mpal15@users.noreply.github.com>
2023-07-25 12:38:11 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/api_hospital')
+mongoose.connect("mongodb+srv://mohitpal9513:mpal9513@cluster0.gbojmyb.mongodb.net/Placement_Cell?retryWrites=true&w=majority")
 .then(()=>{
     console.log("database connected suceefully");
 },(err)=>{
